94d50b90c3bbd067698bd7f5f0e4e7e58a1d5b50
Eduardo Pico <epico@mushroomsoft-it.com>
2024-11-14 12:20:38 -0500

    se agrego la estructura basica de los modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { CategoriaModule } from "./modulos/categoria/categoria.module";
+import { ProductoModule } from "./modulos/producto/producto.module";
+import { ProductorModule } from "./modulos/productor/productor.module";
+import { UsuarioModule } from "./modulos/usuario/usuario.module";
 
 @Module({
-  imports: [],
+  imports: [CategoriaModule, ProductoModule, ProductorModule, UsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
